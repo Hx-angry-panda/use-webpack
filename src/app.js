706655d698638e30cc367c7383bc1bf3c134ccf6
unknown <924193579guge@gmail.com>
2019-09-08 17:10:00 +0800

    整理 dist & src

--- src/app.js
+++
@@ -1,4 +0,0 @@
-import x from './module1'
-x()
-
-import style from './style.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,86 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/app.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
+    entry: './src/js/app.js',
+    output: {
+        filename: './js/bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    
+    
+    //babel-loader
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
         ]
-      }
-    ]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.jsx?$/,
-        use: ['babel-loader', 'astroturf/loader'],
-      }
-    ]
-  }
+    },
+
+    //css-loader
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+
+    //sass-loader
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    
+    //postcss-loader
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", 'sass-loader', "postcss-loader"]
+            }
+        ]
+    },
+
+    //移动到独立分离的 CSS 文件
+    //要放在 postcss-loader 后，因为有执行先后
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
+
+    plugins: [
+        new ExtractTextPlugin("./css/style.css"),
+    ],
 };
-
-
